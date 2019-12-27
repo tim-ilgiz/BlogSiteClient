d3c89503c92c17eef057b4788eaad8f6ac6d1804
politskovoyvo <politskovoyvo@gmail.com>
2019-12-27 15:11:21 +0300

    2

--- ClientAngular/src/app/app-routing-module.ts
+++ ClientAngular/src/app/app-routing-module.ts
@@ -2,10 +2,12 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {WorkSpaceComponent} from "./work-spase/work-space.component";
 import {AuthorizationComponent} from "./authorization/authorization.component";
+import { BoardComponent } from './board/board.component';
 
 const routes: Routes=[
+  {path:'', component: AuthorizationComponent},
   {path:'spaceComponent', component: WorkSpaceComponent},
-  {path:'', component: AuthorizationComponent}
+  {path:'boardComponent', component: BoardComponent}
 ];
 
 @NgModule({
