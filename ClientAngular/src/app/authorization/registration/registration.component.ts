e7cde0acf6c93f0b35b1bf5015c6897c1c6c5fa1
politskovoyvo <v.polickovoy@bm-technology.ru>
2020-01-10 19:56:48 +0300

    адаптивная регистрация

--- ClientAngular/src/app/authorization/registration/registration.component.ts
+++ ClientAngular/src/app/authorization/registration/registration.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css',
+    '../authorization.component.css']
 })
 export class RegistrationComponent implements OnInit {
 
